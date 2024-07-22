136adbe79d5653693f18c0ef1c3e813ecc69b3b7
Ayush <ayush-15@live.com>
2024-07-23 01:38:35 +0530

    setup solana update

--- hello.py
+++
@@ -1,5 +0,0 @@
-import os
-import re
-
-pattern = re.compile(r'^(?!.*\.preview).*\.py$')  # Replace with your regex pattern
-files_to_execute = [f for f in os.listdir('../') if pattern.match(f) and f.endswith('.py')]
